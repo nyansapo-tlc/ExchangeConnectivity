d4d2590619702f938290622d987be4f858abf80d
zeeyyah <fauziya.mudasir@turntabl.io>
2020-11-13 16:32:05 +0000

    connection to mallon exchange apis

---
+++ src/main/java/Nyansapo/exchangeConnectivity/ExchangeConnectivityApplication.java
@@ -0,0 +1,13 @@
+package Nyansapo.exchangeConnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Nyansapo/exchangeConnectivity/ExchangeConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package Nyansapo.exchangeConnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeConnectivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
