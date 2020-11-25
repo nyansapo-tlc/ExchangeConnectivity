a9da774ea79b87ae7a7defaf7800d527b59036e6
Vincent Frimpong <vincent.frimpong@turntabl.io>
2020-11-25 16:12:51 +0000

    Enabled CORS in Trade Controller

--- src/main/java/Nyansapo/exchangeConnectivity/TradeController.java
+++ src/main/java/Nyansapo/exchangeConnectivity/TradeController.java
@@ -3,6 +3,7 @@ package Nyansapo.exchangeConnectivity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class TradeController {
 
