a8afb9717c20fcc6d1d1b0ab89cf9ff4ecd3e2d7
vincent-frimpong <74063336+vincent-frimpong@users.noreply.github.com>
2020-11-27 14:25:01 +0000

    Enabling CORS globally

--- src/main/java/Nyansapo/exchangeConnectivity/ExchangeConnectivityApplication.java
+++ src/main/java/Nyansapo/exchangeConnectivity/ExchangeConnectivityApplication.java
@@ -9,5 +9,15 @@ public class ExchangeConnectivityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeConnectivityApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
 }
