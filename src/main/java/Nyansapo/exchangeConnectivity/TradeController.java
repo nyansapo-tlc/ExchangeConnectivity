305f9d65b810440c8d3fe5c8e1d2873c36a7bcdb
vincent-frimpong <74063336+vincent-frimpong@users.noreply.github.com>
2020-11-27 14:17:46 +0000

    Undo CORS

--- src/main/java/Nyansapo/exchangeConnectivity/TradeController.java
+++ src/main/java/Nyansapo/exchangeConnectivity/TradeController.java
@@ -12,21 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@CrossOrigin
+
 @RestController
 public class TradeController {
 
     @Autowired
     private TradeService tradeService;
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/md")
     public Flux<Trade> getMarketDataFromExchange1(@RequestParam(defaultValue="https://exchange.matraining.com") String exchangePath) {
         Flux<Trade> returnedList = tradeService.getMarketDataFromExchange(exchangePath);
         return returnedList;
     }
     
-    @CrossOrigin(origins = "*")
     @PostMapping(value="/order")
     public Mono<String> sendOrderToMallonExchange(@RequestBody Order order) {
         return tradeService.sendOrderToExchange(order, order.getExchangePath());
